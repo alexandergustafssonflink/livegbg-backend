e5c67f57255c3935627f58c81f419b397c4bb86b
Alexander Gustafsson Flink <alexander@matictribe.com>
2024-04-27 10:00:24 +0200

    added image proxy

---
+++ routes/proxy.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const request = require("request");
+
+router.get("/", (req, res) => {
+  const imageUrl = req.query.url;
+  if (!imageUrl) {
+    return res.status(400).send("Bad Request: URL parameter is missing.");
+  }
+
+  // Skicka förfrågan till den faktiska bildens URL
+  request({ url: imageUrl, encoding: null }, (error, response, body) => {
+    if (error) {
+      return res.status(500).send("Error fetching the image.");
+    }
+    // Skicka rätt content-type
+    res.setHeader("Content-Type", "image/jpeg"); // Du kan behöva justera detta baserat på bildens MIME-typ
+    res.send(body);
+  });
+});
+
+module.exports = router;
